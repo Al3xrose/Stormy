e70cd5c428e2446a194400a4d8ba17e09513fc73
Alph0nse <al3xrose@gmail.com>
2015-06-03 02:39:42 -0700

    Hourly Forecast added

---
+++ app/src/main/java/alphonse/asr/stormy/ui/HourlyForecastActivity.java
@@ -0,0 +1,46 @@
+package alphonse.asr.stormy.ui;
+
+import android.content.Intent;
+import android.os.Parcelable;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.Arrays;
+
+import alphonse.asr.stormy.R;
+import alphonse.asr.stormy.adapters.HourAdapter;
+import alphonse.asr.stormy.weather.Hour;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class HourlyForecastActivity extends ActionBarActivity {
+
+    private Hour[] mHours;
+
+    @InjectView(R.id.recyclerView) RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.inject(this);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
+    }
+
+
+}
--- app/src/main/java/alphonse/asr/stormy/weather/Hour.java
+++ app/src/main/java/alphonse/asr/stormy/weather/Hour.java
@@ -1,9 +1,16 @@
 package alphonse.asr.stormy.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by alphonse on 5/7/2015.
  */
-public class Hour {
+public class Hour implements Parcelable {
     private long mTime;
     private String mSummary;
     private double mTemperature;
@@ -14,8 +21,51 @@ public class Hour {
         return mTime;
     }
 
+    public Hour(){}
+
+    private Hour(Parcel in){
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperature = in.readDouble();
+        mIcon = in.readString();
+        mTimezone = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperature);
+        dest.writeString(mIcon);
+        dest.writeString(mTimezone);
+
+    }
+
+    public static final Parcelable.Creator<Hour> CREATOR
+            = new Parcelable.Creator<Hour>() {
+        public Hour createFromParcel(Parcel in) {
+            return new Hour(in);
+        }
+
+        public Hour[] newArray(int size) {
+            return new Hour[size];
+        }
+    };
+
     public void setTime(long time) {
         mTime = time;
+    }
+
+    public String getHour(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h a");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimezone));
+        Date dateTime = new Date(mTime * 1000);
+        return formatter.format(dateTime);
     }
 
     public String getSummary() {
@@ -26,8 +76,8 @@ public class Hour {
         mSummary = summary;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+        return (int)Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
@@ -41,6 +91,8 @@ public class Hour {
     public void setIcon(String icon) {
         mIcon = icon;
     }
+
+    public int getIconId(){ return Forecast.getIconId(mIcon); }
 
     public String getTimezone() {
         return mTimezone;
